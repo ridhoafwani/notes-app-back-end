8698d29de8600747f9e8231dca042e7a302452b2
ridhoafwani <zikrurridhoafwani@gmail.com>
2023-01-02 01:57:19 +0800

    reconstructure with Hapi plugin

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import Hapi from "@hapi/hapi";
-import routes from "./routes.js";
+import notes from "./api/notes/index.js";
+import NotesService from "./services/inMemory/NotesService.js";
 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -12,10 +14,15 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
   await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+  console.log(`Server berjalan pada host ${server.info.uri}`);
 };
 
 init();
