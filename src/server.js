883c831fd2a1c9e4d0b1881768267182b8318bb0
ridhoafwani <zikrurridhoafwani@gmail.com>
2023-01-08 23:25:16 +0800

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,12 +1,23 @@
+/* eslint-disable max-len */
 import Hapi from "@hapi/hapi";
+
+// notes
 import notes from "./api/notes/index.js";
 import NotesService from "./services/postgres/NotesService.js";
 import NotesValidator from "./validator/notes/index.js";
+
+// users
+import users from "./api/users/index.js";
+import UsersService from "./services/postgres/UsersService.js";
+import UsersValidator from "./validator/users/index.js";
+
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import ClientError from "./exceptions/ClientError.js";
 dotenv.config();
 
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -17,12 +28,51 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
+
+  // Pre Response Error
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+    if (response instanceof Error) {
+      // penanganan client error secara internal.
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+      // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+      if (!response.isServer) {
+        return h.continue;
+      }
+      // penanganan server error sesuai kebutuhan
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+    // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+    return h.continue;
   });
 
   await server.start();
---
+++ src/services/postgres/UsersService.js
@@ -0,0 +1,57 @@
+/* eslint-disable max-len */
+import { nanoid } from "nanoid";
+import pg from "pg";
+const { Pool } = pg;
+import InvariantError from "../../exceptions/InvariantError.js";
+import * as bcrypt from 'bcrypt';
+import NotFoundError from "../../exceptions/NotFoundError.js";
+
+class UsersService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addUser({ username, password, fullname }) {
+    await this.verifyNewUsername(username);
+    const id = `user-${nanoid(16)}`;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const query = {
+      text: 'INSERT INTO users VALUES ($1, $2, $3, $4) RETURNING id',
+      values: [id, username, hashedPassword, fullname],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rowCount) {
+      throw new InvariantError('User gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async verifyNewUsername(username) {
+    const query = {
+      text: 'SELECT * FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+    if (result.rowCount > 0) {
+      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
+    }
+  }
+
+  async getUserById(id) {
+    const query = {
+      text: 'SELECT * FROM users WHERE id = $1',
+      values: [id],
+    };
+    const result = await this._pool.query(query);
+    if (!result.rowCount) {
+      throw new NotFoundError('User tidak ditemukan');
+    }
+    return result.rows[0];
+  }
+}
+
+export default UsersService;
