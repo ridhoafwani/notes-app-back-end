5e705c8e429115bf569e02ec15afaedaced3d218
ridhoafwani <zikrurridhoafwani@gmail.com>
2023-02-06 01:26:36 +0800

    AWS S3 storage implemented

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 import Hapi from "@hapi/hapi";
 import Jwt from '@hapi/jwt';
-import * as path from 'path';
 import Inert from '@hapi/inert';
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
 // notes
 import notes from "./api/notes/index.js";
@@ -30,23 +30,22 @@ import _exports from "./api/exports/index.js";
 import ExportsValidator from "./validator/exports/index.js";
 import ProducerService from "./services/rabbitmq/ProdecerServices.js";
 
-import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import ClientError from "./exceptions/ClientError.js";
+// Uploads
 import uploads from "./api/uploads/index.js";
 import UploadsValidator from "./validator/uploads/index.js";
-import StorageService from "./services/storage/StorageService.js";
+import StorageService from "./services/S3/StorageService.js";
+
+// Error Handler
+import ClientError from "./exceptions/ClientError.js";
 
 
 dotenv.config();
-// const __dirname = path.resolve(path.dirname(''));
-import * as url from 'url';
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const init = async () => {
   const collaborationsService = new CollaborationsService();
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
-  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images'));
+  const storageService = new StorageService();
   const authenticationsService = new AuthenticationsService();
   const server = Hapi.server({
     port: process.env.PORT,
